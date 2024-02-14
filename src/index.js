c31e421d9fad468fd3b3ce61e2d9b7abe8547302
jihkang <106561135+jihkang@users.noreply.github.com>
2024-02-14 16:05:36 +0900

    Feature: router done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
 import { defaultRoutes } from "./router/routes.js";
 
-window.onload = () => {
-  const routes = defaultRoutes();
-  routes.createDefaultRoutes([
-    {
-      path: "hi",
-      children: [
-        {
-          path: "hello",
-          component: "Hello",
-          children: [
-            {
-              path: "world",
-              component: "World",
-            },
-          ],
-        },
-        {
-          path: "bye",
-          component: "Bye",
-        },
-        {
-          path: ":id",
-          component: "ID",
-        },
-      ],
-    },
-  ]);
-  console.log(routes.getCorrectRoutes("hi/hello/world"));
-};
+const routes = defaultRoutes();
+routes.createDefaultRoutes([
+  {
+    path: "hi",
+    children: [
+      {
+        path: "hello",
+        component: "Hello",
+        children: [
+          {
+            path: "world",
+            component: "World",
+          },
+          {
+            path: ":test",
+            component: "Test",
+          },
+          {
+            path: "rr",
+            component: "RR",
+          },
+        ],
+      },
+      {
+        path: "bye",
+        component: "Bye",
+      },
+      {
+        path: ":id",
+        component: "ID",
+        children: [
+          {
+            path: "world",
+            component: "World",
+          },
+          {
+            path: "rr",
+            component: "RR",
+          },
+        ],
+      },
+    ],
+  },
+]);
+// console.log(routes.getCorrectRoutes("hi/hello/test"));
+
+window.addEventListener("hashchange", () => {
+  window.location.hash;
+  console.log(
+    `result:`,
+    routes.getCorrectRoutes(window.location.hash.replace("#", ""))
+  );
+});
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,13 +2,16 @@ export const defaultRoutes = () => {
   let routes = [];
 
   const getCorrectRoutes = (path) => {
-    const result = routes.filter((route) => {
+    const result = routes.find((route) => {
       if (route.path === path) {
         return route;
-      } else if (route.path.includes(":")) {
-        const splitRoute = route.path.split("/");
-        const splitPath = path.split("/");
-        if (splitRoute.length !== splitPath.length) return false;
+      }
+      const splitRoute = route.path.split("/");
+      const splitPath = path.split("/");
+      if (splitRoute.length !== splitPath.length) {
+        return false;
+      }
+      if (route.path.includes(":")) {
         return splitRoute.every((routePath, i) => {
           if (routePath.includes(":")) {
             return true;
@@ -17,29 +20,45 @@ export const defaultRoutes = () => {
         });
       }
     });
+
     return result;
   };
 
-  const createRoutes = (exportRoutes, defaultPath = "") => {
-    if (defaultPath !== "") console.log(defaultPath);
+  const createRoutes = (exportRoutes, defaultPath = "", depths = 0) => {
     return exportRoutes
       .map((route) => {
+        let params;
+        const splitRoute = route.path.split("/");
+        if (route.path.includes(":")) {
+          const temp = {};
+          temp.path = route.path.split(":")[1].split("/")[0];
+          temp.index = depths;
+          params = { ...temp };
+        }
         if (route.children) {
           return [
             {
+              params,
               element: route.element,
               path: defaultPath + route.path + "/",
             },
-            ...createRoutes(route.children, defaultPath + route.path + "/"),
+            ...createRoutes(
+              route.children,
+              defaultPath + route.path + "/",
+              depths + 1
+            ),
           ];
         }
         return {
           ...route,
+          params,
           path: defaultPath + route.path,
+          depths,
         };
       })
       .flat();
   };
+
   const createDefaultRoutes = (router) => {
     routes = createRoutes(router);
   };
